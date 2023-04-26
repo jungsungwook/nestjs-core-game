b0ba0b7834b96423dbcba88101a06670ea5c04a9
jungsungwook <jswcyber@naver.com>
2023-04-26 11:31:49 +0900

    fix: indent

--- src/socket-gateways/chat/gateway.chat.ts
+++ src/socket-gateways/chat/gateway.chat.ts
@@ -21,26 +21,26 @@ export class ChatGateWay {
 
     @SubscribeMessage('send_chat')
     async handleChat(
-        @MessageBody() data: { message: string},
+        @MessageBody() data: { message: string },
         @ConnectedSocket() client: Socket
     ) {
         try {
             const userCustomId = await this.redisService.get(client.id);
-            if(!userCustomId) throw new Error('User not found');
+            if (!userCustomId) throw new Error('User not found');
 
-            if(!data.message) throw new Error('Message is undefined');
-            if(data.message.replace(/\s/g, '').length == 0) throw new Error('Message is empty');
-            if(data.message.length > 100) throw new Error('Message is too long');
-            
+            if (!data.message) throw new Error('Message is undefined');
+            if (data.message.replace(/\s/g, '').length == 0) throw new Error('Message is empty');
+            if (data.message.length > 100) throw new Error('Message is too long');
+
             const returnObj = {
-                type:"message",
+                type: "message",
                 message: data.message,
                 sender: userCustomId,
                 timestamp: new Date().getTime(),
             }
             const chatLog: Object[] = await this.redisService.get('chatLog');
             await this.redisService.set('chatLog', [...chatLog, returnObj]);
-            
+
             this.server.emit('receive_chat', returnObj);
         } catch (err) {
             client.emit('error', err.message);
