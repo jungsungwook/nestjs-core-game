54df4a10a0cea8b4982b143213321fa8ee7564aa
jungsungwook <jswcyber@naver.com>
2023-05-08 14:30:39 +0900

    docs: swagger

--- src/pages/match/dto/match.dto.ts
+++ src/pages/match/dto/match.dto.ts
@@ -1,11 +1,60 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class MatchDto {
+    @ApiProperty({
+        description: '매치 ID',
+        example: '1234567890',
+    })
     match_id: string;
+
+    @ApiProperty({
+        description: '매치 타입',
+        example: 'random_match_1on1',
+    })
     match_type: string;
+
+    @ApiProperty({
+        description: '매치 상태',
+        example: 'match_change',
+    })
     match_status: string;
+
+    @ApiProperty({
+        description: '매치 생성 시간',
+        example: '2021-01-01T00:00:00.000Z',
+    })
     match_start_time: Date;
+
+    @ApiProperty({
+        description: '매치 종료 시간',
+        example: '2021-01-01T00:00:00.000Z',
+    })
     match_end_time?: Date;
+
+    @ApiProperty({
+        description: '매치 랭크 점수',
+        example: 1000,
+    })
     match_rank_score?: number;
+
+    @ApiProperty({
+        description: '매치 참가 유저',
+        example: ['jswcyber', 'abc123'],
+    })
     join_user: string[];
+}
+
+export class DefaultResponseMatchDto {
+    @ApiProperty({
+        description: '응답 코드',
+        example: 200,
+    })
+    statusCode: number;
+
+    @ApiProperty({
+        description: '전달 데이터'
+    })
+    contents: MatchDto;
 }
 
 export enum MatchType {
--- src/pages/match/match.controller.ts
+++ src/pages/match/match.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { MatchService } from "./match.service";
 import { AuthGuard } from "@nestjs/passport";
 import { GetUser } from "src/auth/get-user.decorator";
 import { User } from "../users/user.entity";
+import { DefaultResponseMatchDto, MatchDto } from "./dto/match.dto";
 
 @Controller('match')
 @ApiTags('매칭')
@@ -11,10 +12,19 @@ export class MatchController {
     constructor(private readonly matchService: MatchService) { }
 
     @Get('random-match-1on1-queue')
+    @ApiOperation({
+        summary: '랜덤 매칭 1:1 대기열',
+        description: '랜덤 매칭 1:1 대기열에 참가합니다.'
+    })
+    @ApiResponse({
+        description: '랜덤 매칭 1:1 대기열 참가 성공',
+        type: DefaultResponseMatchDto,
+        status: 200
+    })
     @UseGuards(AuthGuard("jwt"))
     async randomMatch_1on1_queue(
         @GetUser() user: User
-    ) {
+    ): Promise<DefaultResponseMatchDto> {
         try {
             const result = await this.matchService.randomMatch_1on1_queue(user.customId);
             return {
@@ -27,10 +37,19 @@ export class MatchController {
     }
 
     @Post('create-custom-match-1on1')
+    @ApiOperation({
+        summary: '커스텀 매칭 1:1 생성',
+        description: '커스텀 매칭 1:1을 생성합니다.'
+    })
+    @ApiResponse({
+        description: '커스텀 매칭 1:1 생성 성공',
+        type: DefaultResponseMatchDto,
+        status: 200
+    })
     @UseGuards(AuthGuard("jwt"))
     async createMatch_1on1(
         @GetUser() user: User
-    ) {
+    ): Promise<DefaultResponseMatchDto> {
         try {
             const result = await this.matchService.createCustomMatch_1on1(user.customId);
             return {
@@ -44,11 +63,20 @@ export class MatchController {
     }
 
     @Post('join-custom-match-1on1/:matchId')
+    @ApiOperation({
+        summary: '커스텀 매칭 1:1 참가',
+        description: '커스텀 매칭 1:1에 참가합니다.'
+    })
+    @ApiResponse({
+        description: '커스텀 매칭 1:1 참가 성공',
+        type: DefaultResponseMatchDto,
+        status: 200
+    })
     @UseGuards(AuthGuard("jwt"))
     async joinMatch_1on1(
         @GetUser() user: User,
         @Param('matchId') matchId: string
-    ) {
+    ): Promise<DefaultResponseMatchDto> {
         try {
             const result = await this.matchService.joinCustomMatch_1on1(matchId, user.customId);
             return {
@@ -61,6 +89,10 @@ export class MatchController {
     }
 
     @Get('custom-matches')
+    @ApiOperation({
+        summary: '커스텀 매칭 1:1 목록',
+        description: '커스텀 매칭 1:1 목록을 가져옵니다.'
+    })
     async getCustomMatch(
     ) {
         try {
@@ -75,9 +107,13 @@ export class MatchController {
     }
 
     @Get('custom-match/:matchId')
+    @ApiOperation({
+        summary: '커스텀 매칭 1:1 정보',
+        description: '커스텀 매칭 1:1 정보를 가져옵니다.'
+    })
     async getCustomMatchById(
         @Param('matchId') matchId: string
-    ) {
+    ): Promise<DefaultResponseMatchDto> {
         try {
             const result = await this.matchService.getCustomMatch_1on1(matchId);
             return {
@@ -90,10 +126,19 @@ export class MatchController {
     }
 
     @Get('my-match-progress')
+    @ApiOperation({
+        summary: '내 매칭 진행 상황',
+        description: '내 매칭 진행 상황을 가져옵니다.'
+    })
+    @ApiResponse({
+        description: '내 매칭 진행 상황 가져오기 성공',
+        type: DefaultResponseMatchDto,
+        status: 200
+    })
     @UseGuards(AuthGuard("jwt"))
     async getMyMatchProgress(
         @GetUser() user: User
-    ) {
+    ): Promise<DefaultResponseMatchDto> {
         try {
             const result = await this.matchService.getMyMatchProgress("user.customId");
             return {
